9743a2ffb8ea8d34ee0c0b4264257fe739e54a80
Lucas Sedano <lsedano@ewu.edu>
2022-11-15 01:47:59 -0800

    Added Humidity and CSS Changes

--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,10 @@ const city = 'Cheney';
 // Units for Farenheit
 const units = 'imperial';
 
+const humidity = 'humidity';
+
 // URL query string
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${units}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${units}&${humidity}`;
 
 // Using fetch to get data
 fetch(url)
@@ -21,10 +23,11 @@ fetch(url)
   
   // Template to output
   const template = `
-    <h1>Weather</h1>
+    <h1>🌡️Weather</h1>
+    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="Placeholder">
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
-    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
+    Humidity: <data value="${data.main.humidity}" class="humidity">${data.main.humidity}%</data>
   `;
   
   // Insert dynamic template to container
