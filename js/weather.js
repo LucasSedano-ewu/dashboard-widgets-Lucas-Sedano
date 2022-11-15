93d9c5865098f555f0f8ac803a0e43c477b8998e
Lucas Sedano <lsedano@ewu.edu>
2022-11-15 03:00:43 -0800

    CSS Changes and Color, and added Humidity, Wind and Feels Like

--- js/weather.js
+++ js/weather.js
@@ -7,8 +7,12 @@ const units = 'imperial';
 
 const humidity = 'humidity';
 
+const wind = 'wind';
+
+const feelsLike = 'feelsLike';
+
 // URL query string
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${units}&${humidity}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${units}&${humidity}&${wind}&${feelsLike}`;
 
 // Using fetch to get data
 fetch(url)
@@ -24,10 +28,12 @@ fetch(url)
   // Template to output
   const template = `
     <h1>🌡️Weather</h1>
-    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="Placeholder">
+    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
     Humidity: <data value="${data.main.humidity}" class="humidity">${data.main.humidity}%</data>
+    Wind: <data value="${data.wind.speed}" class="wind">${data.wind.speed} mph</data>
+    Feels Like: <data value="${data.main.feels_like}" class="feelsLike">${data.main.feels_like}&#8457;</data>
   `;
   
   // Insert dynamic template to container
